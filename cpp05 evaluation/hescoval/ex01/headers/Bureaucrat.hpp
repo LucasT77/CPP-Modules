dfc60181ac0a7bf7b40792420e2595c5d6d49af6
LucasT77 <74610299+LucasT77@users.noreply.github.com>
2024-08-01 20:15:01 +0100

    Add files via upload

---
+++ cpp05 evaluation/hescoval/ex01/headers/Bureaucrat.hpp
@@ -0,0 +1,48 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include "general.hpp"
+# include "Form.hpp"
+
+class Form;
+
+class Bureaucrat
+{
+	private:
+		string const	_name;
+		int				_grade;
+
+	public:
+
+		Bureaucrat(string const name = "Default Bureaucrat", int grade = 150);
+		Bureaucrat( Bureaucrat const & src );
+		~Bureaucrat();
+
+
+		void			signForm(Form &form);
+
+		string const &	getName() const;
+		int				getGrade() const;
+		void			incrementGrade();
+		void			decrementGrade();
+
+		Bureaucrat &	operator=( Bureaucrat const & rhs );
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+};
+
+std::ostream &				operator<<( std::ostream & o, Bureaucrat const & i );
+
+#endif /* ****************************************************** BUREAUCRAT_H */
