493e5b0dc22dfb27ca3e583911072d0ffba61f6e
LucasT77 <74610299+LucasT77@users.noreply.github.com>
2024-08-09 18:20:05 +0100

    Add files via upload

--- CPP Module 03/ex00/ClapTrap.cpp
+++ CPP Module 03/ex00/ClapTrap.cpp
@@ -5,11 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: luaraujo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/09 16:46:12 by luaraujo          #+#    #+#             */
-/*   Updated: 2024/08/09 16:46:13 by luaraujo         ###   ########.fr       */
+/*   Created: 2024/08/09 18:16:45 by luaraujo          #+#    #+#             */
+/*   Updated: 2024/08/09 18:17:08 by luaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
+ClapTrap::ClapTrap(std::string name)
+{
+    Name = name;
+    hit_points = 10;
+    energy_points = 10;
+    attack_damage = 0;
+}
+ClapTrap::~ClapTrap() {}
 
+
+void    ClapTrap::attack(const std::string& target)
+{
+
+}
+void    ClapTrap::takeDamage(unsigned int amount)
+{
+
+}
+void    ClapTrap::beRepaired(unsigned int amount)
+{
+
+}
