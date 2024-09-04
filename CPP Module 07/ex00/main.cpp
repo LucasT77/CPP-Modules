aaef1a74daac4bf4ddcff5b394b07bcf5b774a65
LucasT77 <74610299+LucasT77@users.noreply.github.com>
2024-09-04 11:49:01 +0100

    Add files via upload

--- CPP Module 07/ex00/main.cpp
+++ CPP Module 07/ex00/main.cpp
@@ -10,4 +10,57 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "whatever.hpp"
 
+void testInt()
+{
+	int a = 5, b = 7;
+	std::cout	<< "/////Test type int/////\n";
+	std::cout	<< "Before swap:\n"
+				<< "a = " << a << std::endl
+				<< "b = " << b << std::endl;
+	swap<int>(&a, &b);
+	std::cout	<< "After swap:\n"
+				<< "a = " << a << std::endl
+				<< "b = " << b << std::endl
+				<< "min(a,b) = " << min<int>(a, b) << std::endl
+				<< "max(a,b) = " << max<int>(a, b) << std::endl;
+}
+
+void testChar()
+{
+	char a = 'x', b = 'y';
+	std::cout	<< "/////Test type char/////\n";
+	std::cout	<< "Before swap:\n"
+				<< "a = " << a << std::endl
+				<< "b = " << b << std::endl;
+	swap<char>(&a, &b);
+	std::cout	<< "After swap:\n"
+				<< "a = " << a << std::endl
+				<< "b = " << b << std::endl
+				<< "min(a,b) = " << min<char>(a, b) << std::endl
+				<< "max(a,b) = " << max<char>(a, b) << std::endl;
+}
+
+void testString()
+{
+	std::string a = "Hello";
+	std::string b = "World";
+	std::cout	<< "/////Test type std::string/////\n";
+	std::cout	<< "Before swap:\n"
+				<< "a = " << a << std::endl
+				<< "b = " << b << std::endl;
+	swap<std::string>(&a, &b);
+	std::cout	<< "After swap:\n"
+				<< "a = " << a << std::endl
+				<< "b = " << b << std::endl
+				<< "min(a,b) = " << ::min<std::string>(a, b) << std::endl
+				<< "max(a,b) = " << ::max<std::string>(a, b) << std::endl;
+}
+
+int main()
+{
+	testInt();
+	testChar();
+	testString();
+}
