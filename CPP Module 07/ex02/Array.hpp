eb0361dda84963cbfd58a627bcc950d1451261f7
LucasT77 <74610299+LucasT77@users.noreply.github.com>
2024-09-05 16:54:41 +0100

    Add files via upload

--- CPP Module 07/ex02/Array.hpp
+++ CPP Module 07/ex02/Array.hpp
@@ -10,4 +10,106 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
 
+#include <iostream>
+
+template <typename T>
+class Array
+{
+	private:
+		T *arr;
+		unsigned int _size;
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(Array &other);
+		Array &operator=(const Array &other);
+		~Array();
+
+		class IndexOutOfBounds : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		unsigned int size() const;
+		void printAll();
+		T &operator[](unsigned int index);
+};
+
+///////Contructors///////
+
+template <typename T>
+Array<T>::Array()
+{
+	arr = new T[0];
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n)
+{
+	_size = n;
+	arr = new T[_size];
+}
+
+template <typename T>
+Array<T>::Array(Array &other)
+{
+	*this = other;
+}
+
+///////Destructors///////
+
+template <typename T>
+Array<T>::~Array() 
+{
+	delete [] arr;
+}
+
+///////Operator overloading///////
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array &other)
+{
+	delete [] arr;
+	this->_size = other.size();
+	arr = new T[_size];
+	for (unsigned int i = 0; i < other._size; i++)
+		arr[i] = other.arr[i];
+	return (*this);
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int index)
+{
+	if (index >= _size)
+		throw Array<T>::IndexOutOfBounds();
+	return arr[index];
+}
+
+///////Exception///////
+
+template <typename T>
+const char *Array<T>::IndexOutOfBounds::what() const throw()
+{
+    return "Index out of bounds!\n";
+}
+
+///////Methods///////
+
+template <typename T>
+unsigned int Array<T>::size() const
+{
+	return this->_size;
+}
+
+template <typename T>
+void Array<T>::printAll()
+{
+	for(unsigned int i = 0; i < _size; i++)
+		std::cout << "[" << i << "]" << arr[i] << std::endl;
+}
+
+#endif
--- CPP Module 07/ex02/main.cpp
+++ CPP Module 07/ex02/main.cpp
@@ -10,4 +10,90 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
 
+int main()
+{
+	/*Array<int>	a(3);
+	Array<int>	b(3);
+
+	for (size_t i = 0; i < a.size(); i += 1) {
+		std::cout << "a[" << i << "] = " << a[i] << "\n";
+	}
+	std::cout << std::endl;
+	for (size_t i = 0; i < b.size(); i += 1) {
+		std::cout << "b[" << i << "] = " << b[i] << "\n";
+	}
+	a[0] = 1;
+	a[1] = 1;
+	a[2] = 1;
+	std::cout << std::endl;
+	std::cout << "AFTER FILLING A WITH 1's" << std::endl;
+	for (size_t i = 0; i < a.size(); i += 1) {
+		std::cout << "a[" << i << "] = " << a[i] << "\n";
+	}
+	std::cout << std::endl;
+	for (size_t i = 0; i < b.size(); i += 1) {
+		std::cout << "b[" << i << "] = " << b[i] << "\n";
+	}
+	b = a;
+
+	std::cout << "AFTER B = A" << std::endl;
+	for (size_t i = 0; i < a.size(); i += 1) {
+		std::cout << "a[" << i << "] = " << a[i] << "\n";
+	}
+	std::cout << std::endl;
+	for (size_t i = 0; i < b.size(); i += 1) {
+		std::cout << "b[" << i << "] = " << b[i] << "\n";
+	}
+
+
+    // Empty Array
+    Array<int> empty;
+
+	// OUT OF BOUNDS EXCEPTION
+    try {
+        std::cout << a[a.size() + 1] << std::endl;
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }*/
+
+	Array<int> x(3);
+	Array<int> y(x);
+	Array<int> z;
+
+	x[0] = 10;
+	x[1] = 20;
+	x[2] = 30;
+	
+	y[0] = 100;
+	y[1] = 200;
+	y[2] = 300;
+
+	try
+	{
+		z[0] = 1000;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	z = x;
+
+	std::cout << "x[0] = " << x[0] << "\n\n"; 
+
+	std::cout << "x:\n";
+	x.printAll();
+	std::cout << std::endl;
+
+	std::cout << "y:\n";
+	y.printAll();
+	std::cout << std::endl;
+
+	std::cout << "z:\n";
+	std::cout << "z.size() = " << z.size() << '\n';
+	z.printAll();
+	std::cout << std::endl;
+}
