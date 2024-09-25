40361814adbea4526d39c765e783fefed4426b02
LucasT77 <74610299+LucasT77@users.noreply.github.com>
2024-09-25 16:24:53 +0100

    Add files via upload

--- CPP Module 08/ex02/MutantStack.hpp
+++ CPP Module 08/ex02/MutantStack.hpp
@@ -10,4 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
 
+#include <iostream>
+
+template <typename T>
+class MutantStack
+{
+	private:
+		std::stack<T> _stack;
+
+	public:
+		MutantStack();
+		MutantStack(MutantStack &other);
+		MutantStack &operator=(const MutantStack &other);
+		~MutantStack();
+};
+
+///////Contructors & Destructor///////
+
+template <typename T>
+MutantStack<T>::MutantStack()
+{
+	
+}
+
+template <typename T>
+MutantStack<T>::MutantStack(MutantStack &other)
+{
+	*this = other;
+}
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack &other)
+{
+	this->_stack = other._stack;
+	return (*this);
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack() 
+{
+	
+}
+
+# endif
