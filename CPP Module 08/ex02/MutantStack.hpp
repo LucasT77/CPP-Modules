64199079ba0688b79e5b03aa823313ec668d321d
LucasT77 <74610299+LucasT77@users.noreply.github.com>
2024-09-25 22:19:32 +0100

    Update MutantStack.hpp

--- CPP Module 08/ex02/MutantStack.hpp
+++ CPP Module 08/ex02/MutantStack.hpp
@@ -14,6 +14,7 @@
 # define MUTANTSTACK_HPP
 
 #include <iostream>
+#include <stack>
 
 template <typename T>
 class MutantStack
@@ -35,11 +36,22 @@ class MutantStack
 		void push(T arg);
 		void pop();
 		void swap(MutantStack &other);
-		void emplace(T arg);	
-		
-		class Iterator
-		{
+		void emplace(T arg);
 
+		//Iterator Methods
+		T *begin();
+		T *end();	
+		
+		class iterator
+		{
+			public:
+				T *it;
+
+				iterator &operator=(T *ptr);
+				bool operator==(MutantStack<T>::iterator other);
+				bool operator!=(MutantStack<T>::iterator other);
+				iterator &operator++(void);
+				iterator &operator--(void);
 		};
 };
 
@@ -70,4 +82,65 @@ MutantStack<T>::~MutantStack()
 	
 }
 
-# endif
+///////Stack Original Methods///////
+
+template <typename T>
+T				MutantStack<T>::top() {return _stack.top();}
+template <typename T>
+bool 			MutantStack<T>::empty() {return _stack.empty();}
+template <typename T>
+unsigned int	MutantStack<T>::size() {return _stack.size();}
+template <typename T>
+void			MutantStack<T>::push(T arg) {_stack.push(arg)}
+template <typename T>
+void			MutantStack<T>::pop() {_stack.pop()}
+template <typename T>
+void			MutantStack<T>::swap(MutantStack &other) {this->_stack.swap(other._stack)}
+template <typename T>
+void			MutantStack<T>::emplace(T arg) {_stack.emplace(arg)}
+
+///////Iterator Methods///////
+
+template <typename T>
+T	*MutantStack<T>::begin() {return _stack;}
+
+template <typename T>
+T	*MutantStack<T>::end() {return _stack + (_stack.size() - 1);}
+
+///////Iterator Overload///////
+
+template <typename T>
+MutantStack<T>::iterator &MutantStack<T>::iterator::operator=(T *ptr)
+{
+	this->it = ptr;
+	return (*this);
+}
+
+template <typename T>
+bool MutantStack<T>::iterator::operator==(MutantStack<T>::iterator other)
+{
+	return (this->it == other.it)
+}
+
+template <typename T>
+bool MutantStack<T>::iterator::operator!=(MutantStack<T>::iterator other)
+{
+	return (this->it != other.it)
+}
+
+template <typename T>
+MutantStack<T>::iterator &MutantStack<T>::iterator::operator++(void)
+{
+	++this->it;
+	return (*this);
+}
+
+template <typename T>
+MutantStack<T>::iterator &MutantStack<T>::iterator::operator--(void)
+{
+	--this->it;
+	return (*this);
+}
+
+
+#endif
